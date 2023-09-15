1f8b6aaeca31233b3d3b0ee6fb89100aa9920dc3
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-09-15 20:23:20 +0300

    schema

--- schema.sql
+++ schema.sql
@@ -6,41 +6,42 @@ DROP TABLE IF EXISTS bids;
 DROP TABLE IF EXISTS messages;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY, 
-    username VARCHAR(12) NOT NULL, 
-    password TEXT NOT NULL,
-    mail TEXT NOT NULL,
-    created_at TIMESTAMP, 
-    last_modified TIMESTAMP, 
-    admin BOOLEAN DEFAULT False
+	id SERIAL PRIMARY KEY, 
+        username TEXT UNIQUE,
+        password TEXT NOT NULL,
+        mail TEXT NOT NULL,
+        created_at TIMESTAMP, 
+        last_modified TIMESTAMP, 
+        admin BOOLEAN DEFAULT False
 );
 
 CREATE TABLE auctions (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users,
-	title VARCHAR(40) NOT NULL,
-	content VARCHAR(200),
-	_class VARCHAR(20),
-	condition VARCHAR(20),
-	city VARCHAR(20),
+	title TEXT UNIQUE,
+	content TEXT,
+	_class TEXT,
+	condition TEXT,
+	city TEXT,
 	bid_start NUMERIC DEFAULT 1,
-	created_at TIMESTAMP, 
+	created_at TIMESTAMP NOT NULL, 
 	ending_time TIMESTAMP NOT NULL,
 	active BOOLEAN DEFAULT True,
-	winner_id INTEGER, 
+	winner_id INTEGER REFERENCES users, 
 	solved  BOOLEAN DEFAULT False
 );
 
 CREATE TABLE images (
-    id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	auction_id INTEGER REFERENCES auctions, 
 	name TEXT, 
+	frontside BOOLEAN,
 	data BYTEA);
 	
 CREATE TABLE feedbacks (
 	id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users,
-	feedback VARCHAR(200),
+	feedback TEXT,
 	feedback_time TIMESTAMP 
 );
 
@@ -57,8 +58,8 @@ CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
 	user_id_from INTEGER REFERENCES users,
 	user_id_to INTEGER REFERENCES users,
-	message VARCHAR(100),
+	message TEXT,
 	message_sent TIMESTAMP
 );
 
-INSERT INTO users (username, password,mail, created_at, last_modified, admin) VALUES ("Huuto", "Kauppa", "admin@korttihuutokauppa.fi", NOW(), NOW(), False);
+INSERT INTO users (username, password,mail, created_at, last_modified, admin) VALUES ('Admin', 'adminword', 'admin@admin.com', NOW(), NOW(), True);
